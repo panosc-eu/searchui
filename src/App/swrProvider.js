26f39a1990557d449cd71738bdce21d8b80bfda5
Jiří Majer <juras.majer@gmail.com>
2021-08-10 08:48:27 +0200

    cleanup
    forgot to remove keycloak stuff from here

--- src/App/swrProvider.js
+++ src/App/swrProvider.js
@@ -1,26 +1,15 @@
 import React from 'react'
 
-import {useKeycloak} from '@react-keycloak/web'
 import {SWRConfig} from 'swr'
 
-//not happy with the state of data fetching at this point
 const SWRProvider = ({children}) => {
-  const {keycloak} = useKeycloak()
-  const getUrlWithBase = (url) =>
-    url.startsWith('/account') || url.startsWith('/plans')
-      ? keycloak.authenticated
-        ? process.env.REACT_APP_API + url
-        : false
-      : (process.env.REACT_APP_SEARCH ?? 'http://localhost:5000/api') + url
+  const getUrl = (url) =>
+    (process.env.REACT_APP_SEARCH ?? 'http://localhost:5000/api') + url
   const fetcher = (url) => {
-    const access_token = keycloak?.token ?? ''
     const method = url.endsWith('token') ? 'post' : 'get'
     return (
       url &&
-      fetch(getUrlWithBase(url), {
-        headers: {
-          access_token,
-        },
+      fetch(getUrl(url), {
         method,
       }).then((r) => r.json())
     )
