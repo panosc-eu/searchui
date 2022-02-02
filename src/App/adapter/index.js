73d41099eef874681bdacb562aca48cd16dd158c
Jiri Majer <jirimajer@me.com>
2022-02-02 08:48:20 +0100

    formatting fix

--- src/App/adapter/index.js
+++ src/App/adapter/index.js
@@ -1,10 +1,14 @@
-import
-  {mergeState, parseState, buildIncludeKey, buildWhereKey, stripEmptyKeys}
-    from "./BuildingBlocks"
+import {
+  mergeState,
+  parseState,
+  buildIncludeKey,
+  buildWhereKey,
+  stripEmptyKeys,
+} from './BuildingBlocks';
 const main = (initialState, diffState = []) => {
   const state = mergeState(initialState, diffState);
   const [groups, config] = parseState(state);
-  const include = buildIncludeKey(groups)
+  const include = buildIncludeKey(groups);
   const where = buildWhereKey(groups);
   const query = stripEmptyKeys({
     include,
