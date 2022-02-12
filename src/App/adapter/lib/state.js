1a24eb6dbd22ef6bd556ce587fe261fedbbf0d62
Jiri Majer <jirimajer@me.com>
2022-02-12 17:32:37 +0100

    some more tests

--- src/App/adapter/lib/state.js
+++ src/App/adapter/lib/state.js
@@ -1,15 +1,16 @@
-import OPERATORS from '../operators.json';
-import MAP from '../targets.json';
+import OPERATORS from '../operators.json'
+import MAP from '../targets.json'
 
-export const FALLBACK_GROUP_OPERATOR = 'and';
-export const LABEL_FOR_CONFIG = 'c';
+export const FALLBACK_GROUP_OPERATOR = 'and'
+export const LABEL_FOR_CONFIG = 'c'
 
-const resolveOperator = (label) => OPERATORS[label] || FALLBACK_GROUP_OPERATOR;
+export const resolveOperatorFrom = (operators) => (label) =>
+  operators[label] || FALLBACK_GROUP_OPERATOR
 
-const resolvePath = (label, endpoint) => {
-  const known = MAP[label];
-  const nested = known?.[endpoint];
-  const fallback = known?.fallback;
+export const resolvePathFrom = (map) => (label, endpoint) => {
+  const known = map[label]
+  const nested = known?.[endpoint]
+  const fallback = known?.fallback
 
   return label === endpoint
     ? null
@@ -19,21 +20,27 @@ const resolvePath = (label, endpoint) => {
     ? [...fallback, label]
     : known
     ? [label]
-    : null;
-};
+    : null
+}
 
-const createGroupFor = (endpoint) => (obj) => {
-  const {
-    label,
-    value,
-    operator = value || resolveOperator(label),
-    target = resolvePath(label, endpoint),
-  } = obj;
-  return { label, target, operator };
-};
+const resolvePath = resolvePathFrom(MAP)
+const resolveOperator = resolveOperatorFrom(OPERATORS)
 
-const enhancePagination = (config) => {
-  const { page, pageSize } = config;
+export const createGroupForUsing =
+  (resolveOperatorFn, resolveTargetFn) => (endpoint) => (obj) => {
+    const {
+      label,
+      value,
+      operator = resolveOperatorFn(label),
+      target = resolveTargetFn(label, endpoint),
+    } = obj
+    return { label, target, operator: value || operator }
+  }
+
+const createGroupFor = createGroupForUsing(resolveOperator, resolvePath)
+
+export const enhancePagination = (config) => {
+  const { page, pageSize } = config
   const obj =
     pageSize && page
       ? {
@@ -41,49 +48,49 @@ const enhancePagination = (config) => {
           limit: pageSize,
           skip: pageSize * (page - 1),
         }
-      : config;
-  const { page: _, pageSize: __, ...res } = obj;
-  return res;
-};
+      : config
+  const { page: _, pageSize: __, ...res } = obj
+  return res
+}
 
 export function parseState(state, endpoint) {
-  const createGroup = createGroupFor(endpoint);
-  const config = state.find(({ label }) => label === LABEL_FOR_CONFIG);
+  const createGroup = createGroupFor(endpoint)
+  const config = state.find(({ label }) => label === LABEL_FOR_CONFIG)
   const {
     label: _,
     name: __,
     value: ___,
     include: included,
     ...base
-  } = enhancePagination(config);
+  } = enhancePagination(config)
 
-  const targets = state.reduce((acc, scope) => {
-    const { group } = scope;
+  const uniqueTargets = state.reduce((acc, scope) => {
+    const { group } = scope
     if (acc.includes(group) || !group) {
-      return acc;
+      return acc
     }
-    return [...acc, group];
-  }, included);
+    return [...acc, group]
+  }, included)
 
-  const groups = targets
+  const groups = uniqueTargets
     .map((label) =>
-      createGroup(state.find((obj) => obj.label === label) || { label })
+      createGroup(state.find((obj) => obj.label === label) || { label }),
     )
     .map((obj) => ({
       ...obj,
       filters: state.filter(({ group }) => group === obj.label),
-    }));
+    }))
 
-  const include = groups.filter((group) => group.target);
-  const where = groups.find((group) => group.label === endpoint);
+  const include = groups.filter((group) => group.target)
+  const where = groups.find((group) => group.label === endpoint)
 
-  return [include, where, base];
+  return [include, where, base]
 }
 
 const squash = (list) => {
-  const pairs = list.flatMap((obj) => Object.entries(obj));
-  return pairs.reduce((acc, scope) => ({ ...acc, [scope[0]]: scope[1] }), {});
-};
+  const pairs = list.flatMap((obj) => Object.entries(obj))
+  return pairs.reduce((acc, scope) => ({ ...acc, [scope[0]]: scope[1] }), {})
+}
 
 const groupByLabel = (list) =>
   list.reduce(
@@ -91,16 +98,16 @@ const groupByLabel = (list) =>
       ...acc,
       [scope.label]: [...(acc[scope.label] || []), scope],
     }),
-    {}
-  );
+    {},
+  )
 
 export function mergeState(inits, diffs) {
   const filteredInits = inits.filter(
     (obj) =>
       obj.label === LABEL_FOR_CONFIG ||
       diffs.map((o) => o.label).includes(obj.label) ||
-      diffs.map((o) => o.group).includes(obj.label)
-  );
-  const byLabel = groupByLabel([...filteredInits, ...diffs]);
-  return Object.entries(byLabel).flatMap(([, a]) => squash(a));
+      diffs.map((o) => o.group).includes(obj.label),
+  )
+  const byLabel = groupByLabel([...filteredInits, ...diffs])
+  return Object.entries(byLabel).flatMap(([, a]) => squash(a))
 }
--- src/App/adapter/lib/state.test.js
+++ src/App/adapter/lib/state.test.js
@@ -1,12 +1,103 @@
-import { mergeState, parseState, LABEL_FOR_CONFIG } from './state';
+import {
+  mergeState,
+  parseState,
+  resolveOperatorFrom,
+  resolvePathFrom,
+  createGroupForUsing,
+  enhancePagination,
+  LABEL_FOR_CONFIG,
+  FALLBACK_GROUP_OPERATOR,
+} from './state'
+
+//bellow is also needed to test createGroupForUsing
+const map = {
+  techniques: {
+    datasets: [],
+    fallback: ['datasets'],
+  },
+}
+const resolvePath = resolvePathFrom(map)
+
+const operators = {
+  techniques: 'or',
+}
+const resolveOperator = resolveOperatorFrom(operators)
+
+describe('resolvePathFrom(map)(label, endpoint)', () => {
+  it('gets path for endpoint', () => {
+    expect(resolvePath('techniques', 'datasets')).toEqual(['techniques'])
+  })
+  it('gets fallback path', () => {
+    expect(resolvePath('techniques', 'documents')).toEqual([
+      'datasets',
+      'techniques',
+    ])
+  })
+  it('is null while label and endpoint match', () => {
+    expect(resolvePath('datasets', 'datasets')).toBeNull()
+  })
+  it('is null if target is not found in map', () => {
+    expect(resolvePath('parameters', 'documents')).toBeNull()
+  })
+})
+
+describe('resolveOperatorFrom(operators)(label)', () => {
+  it('returns operator for given label', () => {
+    expect(resolveOperator('techniques')).toBe('or')
+  })
+  it('returns fallback operator if none is found for given label', () => {
+    expect(resolveOperator('parameters')).toBe(FALLBACK_GROUP_OPERATOR)
+  })
+})
+
+describe('createGroupFor(endpoint)(object)', () => {
+  const endpoint = 'documents'
+  const createGroup = createGroupForUsing(
+    resolveOperator,
+    resolvePath,
+  )(endpoint)
+
+  it('adds operator and target', () => {
+    const group = { label: 'techniques' }
+    const expected = {
+      label: 'techniques',
+      operator: 'or',
+      target: ['datasets', 'techniques'],
+    }
+    expect(createGroup(group)).toEqual(expected)
+  })
+
+  it('doesnt write over existing keys', () => {
+    const group = {
+      label: 'techniques',
+      operator: 'foo',
+      target: ['bar', 'baz'],
+    }
+    expect(createGroup(group)).toEqual(group)
+  })
+
+  it('key value overrides operator', () => {
+    const group = {
+      label: 'techniques',
+      operator: 'or',
+      value: 'and',
+    }
+    const expected = {
+      label: 'techniques',
+      operator: 'and',
+      target: ['datasets', 'techniques'],
+    }
+    expect(createGroup(group)).toEqual(expected)
+  })
+})
 
 describe('mergeState(inits, diffs)', () => {
-  test('Works on empty arrays', () => {
-    const inits = [];
-    const diffs = [];
-    expect(mergeState(inits, diffs)).toHaveLength(0);
-  });
-  test('Merges objects with matching labels', () => {
+  it('Works on empty arrays', () => {
+    const inits = []
+    const diffs = []
+    expect(mergeState(inits, diffs)).toHaveLength(0)
+  })
+  it('Merges objects with matching labels', () => {
     const inits = [
       {
         label: '1',
@@ -16,7 +107,7 @@ describe('mergeState(inits, diffs)', () => {
         label: '2',
         group: 'test',
       },
-    ];
+    ]
     const diffs = [
       {
         label: '1',
@@ -26,7 +117,7 @@ describe('mergeState(inits, diffs)', () => {
         label: '2',
         value: 'something',
       },
-    ];
+    ]
     const expected = [
       {
         label: '1',
@@ -38,33 +129,33 @@ describe('mergeState(inits, diffs)', () => {
         group: 'test',
         value: 'something',
       },
-    ];
-    expect(mergeState(inits, diffs)).toEqual(expected);
-  });
-  test('Merges "right"', () => {
+    ]
+    expect(mergeState(inits, diffs)).toEqual(expected)
+  })
+  it('Merges "right"', () => {
     const inits = [
       {
         label: '1',
         group: 'test',
         value: '',
       },
-    ];
+    ]
     const diffs = [
       {
         label: '1',
         value: 'something',
       },
-    ];
+    ]
     const expected = [
       {
         label: '1',
         value: 'something',
         group: 'test',
       },
-    ];
-    expect(mergeState(inits, diffs)).toEqual(expected);
-  });
-  test('Only filters found in diffs get through', () => {
+    ]
+    expect(mergeState(inits, diffs)).toEqual(expected)
+  })
+  it('Only filters found in diffs get through', () => {
     const inits = [
       {
         label: '1',
@@ -77,13 +168,13 @@ describe('mergeState(inits, diffs)', () => {
         group: 'test',
         operator: 'like',
       },
-    ];
+    ]
     const diffs = [
       {
         label: '1',
         value: 'something',
       },
-    ];
+    ]
     const expected = [
       {
         label: '1',
@@ -91,25 +182,25 @@ describe('mergeState(inits, diffs)', () => {
         operator: 'like',
         value: 'something',
       },
-    ];
-    expect(mergeState(inits, diffs)).toEqual(expected);
-  });
-  test('Config is never filtered out', () => {
-    const inits = [{ label: LABEL_FOR_CONFIG }];
-    const diffs = [];
-    expect(mergeState(inits, diffs)).toEqual(inits);
-  });
-  test('Objects (= groups) whose labels match the group key of any object in diffs are not filtered out', () => {
-    const inits = [{ label: 'group' }];
+    ]
+    expect(mergeState(inits, diffs)).toEqual(expected)
+  })
+  it('Config is never filtered out', () => {
+    const inits = [{ label: LABEL_FOR_CONFIG }]
+    const diffs = []
+    expect(mergeState(inits, diffs)).toEqual(inits)
+  })
+  it('Objects whose labels match the group key of any object in diffs are not filtered out', () => {
+    const inits = [{ label: 'group' }]
     const diffs = [
       {
         label: 'filter',
         group: 'group',
       },
-    ];
-    const expected = [...inits, ...diffs];
-    expect(mergeState(inits, diffs)).toEqual(expected);
-  });
-});
+    ]
+    const expected = [...inits, ...diffs]
+    expect(mergeState(inits, diffs)).toEqual(expected)
+  })
+})
 
-describe('parseState(state, endpoint)', () => {});
+describe('parseState(state, endpoint)', () => {})
