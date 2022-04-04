6089dcf0c586d3ff6c043f95519773f8eb11e5fd
Jiri Majer <juras.majer@gmail.com>
2022-04-04 14:58:10 +0200

    Remove incorrect CSS

--- src/Explore/DocumentItem.js
+++ src/Explore/DocumentItem.js
@@ -31,7 +31,6 @@ function DocumentItem(props) {
           target="_blank"
           sx={{
             textDecoration: 'none',
-            curosor: 'pointer',
           }}
         >
           {title}
