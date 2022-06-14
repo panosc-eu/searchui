143719aaf198eb0bf04136c50d09004875c051a5
Jiri Majer <juras.majer@gmail.com>
2022-06-14 13:21:25 +0200

    Move query inspector

--- src/Explore/DocumentList.js
+++ src/Explore/DocumentList.js
@@ -1,25 +1,22 @@
 import React, { useEffect } from 'react'
 
-import useApi, { useQuery } from '../Api/useApi'
+import useApi from '../Api/useApi'
 import useFilters from '../Api/useFilters'
 import { useSearchStore } from '../App/stores'
 import { Flex, Card, Text, Heading } from '../Primitives'
 import DocumentItem from './DocumentItem'
-import QueryDebug from './QueryDebug'
 
 function DocumentList() {
   const setCount = useSearchStore((state) => state.setCount)
   const filters = useFilters()
   const { data } = useApi('/documents', filters)
-  const query = useQuery('/documents', filters)
 
   useEffect(() => {
     setCount(data.length)
   }, [data, setCount])
 
   return (
-    <>
-      <Flex column gap={[3, 3, 3, 4]}>
+    <Flex column gap={[3, 3, 3, 4]}>
         {data.length === 0 ? (
           <Card p={[3, 4]}>
             <Heading>No results</Heading>
@@ -31,8 +28,6 @@ function DocumentList() {
           ))
         )}
       </Flex>
-      <QueryDebug query={JSON.stringify(query)} />
-    </>
   )
 }
 export default DocumentList
--- src/Explore/ExplorePage.js
+++ src/Explore/ExplorePage.js
@@ -2,6 +2,8 @@ import React, { useEffect, Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { useLocation } from 'react-router-dom'
 
+import { useQuery } from '../Api/useApi'
+import useFilters from '../Api/useFilters'
 import ErrorFallback from '../App/ErrorFallback'
 import ResultsCount from '../App/ResultsCount'
 import Spinner from '../App/Spinner'
@@ -9,6 +11,7 @@ import { useSearchStore } from '../App/stores'
 import { Flex, Box } from '../Primitives'
 import Search from '../Search/Search'
 import DocumentList from './DocumentList'
+import QueryDebug from './QueryDebug'
 
 function ExplorePage(props) {
   const { isDesktop } = props
@@ -16,50 +19,55 @@ function ExplorePage(props) {
   const { search } = useLocation()
   const setSearch = useSearchStore((state) => state.setSearch)
 
+  const filters = useFilters()
+  const query = useQuery('/documents', filters)
   useEffect(() => {
     setSearch(search)
   }, [search, setSearch])
 
   return (
-    <Flex flexDirection={['column', 'column', 'row']} gap={[3, 3, 3, 4]}>
-      {isDesktop ? (
-        <Box
-          as="aside"
-          display={['none', 'none', 'block']}
-          width={[1, 1, 1 / 4]}
-        >
-          {isDesktop && <Search />}
-        </Box>
-      ) : (
-        <Flex alignItems="center">
+    <>
+      <Flex flexDirection={['column', 'column', 'row']} gap={[3, 3, 3, 4]}>
+        {isDesktop ? (
           <Box
-            as="details"
-            sx={{
-              flex: '1 1 0%',
-              display: ['block', 'block', 'none'],
-              width: [1, 1, 1 / 4],
-            }}
+            as="aside"
+            display={['none', 'none', 'block']}
+            width={[1, 1, 1 / 4]}
           >
-            <Box as="summary" sx={{ fontSize: 3, cursor: 'pointer' }}>
-              Filters
-            </Box>
-            <Box mt={2}>
-              <Search />
-            </Box>
+            {isDesktop && <Search />}
           </Box>
-          <Box sx={{ fontSize: 2 }}>
-            <ResultsCount />
-          </Box>
-        </Flex>
-      )}
-      <Box width={[1, 1, 3 / 4]}>
-        <ErrorBoundary FallbackComponent={ErrorFallback} resetKeys={[search]}>
-          <Suspense fallback={<Spinner />}>
-            <DocumentList />
-          </Suspense>
-        </ErrorBoundary>
-      </Box>
-    </Flex>
+        ) : (
+          <Flex alignItems="center">
+            <Box
+              as="details"
+              sx={{
+                flex: '1 1 0%',
+                display: ['block', 'block', 'none'],
+                width: [1, 1, 1 / 4],
+              }}
+            >
+              <Box as="summary" sx={{ fontSize: 3, cursor: 'pointer' }}>
+                Filters
+              </Box>
+              <Box mt={2}>
+                <Search />
+              </Box>
+            </Box>
+            <Box sx={{ fontSize: 2 }}>
+              <ResultsCount />
+            </Box>
+          </Flex>
+        )}
+        <Box width={[1, 1, 3 / 4]}>
+          <ErrorBoundary FallbackComponent={ErrorFallback} resetKeys={[search]}>
+            <Suspense fallback={<Spinner />}>
+              <DocumentList />
+            </Suspense>
+          </ErrorBoundary>
+        </Box>
+      </Flex>
+      <QueryDebug query={JSON.stringify(query)} />
+    </>
   )
 }
 
